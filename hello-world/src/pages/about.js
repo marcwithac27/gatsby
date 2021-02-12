279497697454c0f05c0d36fd26cacd3548091290
Marc Maldonado <marc@telecomelite.com>
2021-02-12 15:29:44 -0500

    got it done

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Link to="/">Home</Link>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="About Gatsby" />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
